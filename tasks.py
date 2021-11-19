3b2b528998aefd5f6f28dea2f4408912f4a6d051
pablo <PRM00@protonmail.com>
2021-11-19 10:11:25 +0100

    Cambio de gestor de tareas taskipy a invoke
    Tambien se ha añadido una función para la comprobación de la sintaxis

---
+++ tasks.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+
+from invoke import task
+from invoke import run
+
+@task
+def check(c):
+    cmd= "python -m compileall -q serieMotion"
+    print("Realizando check de la sintaxis...\n\n")
+    result = c.run(cmd, hide=False, warn=False)
+    if result.ok:
+        print("Check realizado exitosamente")
+
+# =============================================================================
+# @task
+# def run(c):
+#
+# @task
+# def tests(c):
+#   cmd = "python -m pytest"
+#
+# =============================================================================

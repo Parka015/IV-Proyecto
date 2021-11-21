550f3c5827084cb651b2fac0446cb6bbe145e9fd
pablo <PRM00@protonmail.com>
2021-11-21 20:59:42 +0100

    Actualización de la ruta de los fuentes
    Recordemos que este archivo lo usa invoke para realizar las tareas

--- tasks.py
+++ tasks.py
@@ -7,7 +7,7 @@ from invoke import run
 
 @task
 def check(c):
-    cmd= "python -m compileall -q serieMotion"
+    cmd= "python -m compileall -q source"
     print("Realizando check de la sintaxis...\n\n")
     result = c.run(cmd, hide=False, warn=False)
     if result.ok:

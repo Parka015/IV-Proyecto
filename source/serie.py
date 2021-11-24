608bcda94664a15b7f918164dd786e6545b5fc03
Ignacio Carvajal Herrera <90694122+NachoCarher@users.noreply.github.com>
2021-11-24 16:54:04 +0100

    Implementacion de la estructura de datos para M0
    Se ha creado la estructura de datos para una serie y se avanza en la solución de [Tiene que haber una manera de identificar las series de forma única](#7) asociada a HU1(#3)

--- source/serie.py
+++ source/serie.py
@@ -1,2 +1,17 @@
+# -*- coding: utf-8 -*-
+# Clase que almacena la información correspondiente a una serie
 
-#Por desarrollar (Nacho)
+Class serie:
+  def __init__(self, iden, val):
+    self.identificador = iden   # Codigo para identificar de forma unica cada serie
+    self.valoracion = val       # Valor real entre 0.0 y 5.0 que determina la puntiación de una serie
+    
+  # GETTERS  
+    
+  def getId(self):
+    return self.identificador
+  
+  def getValoracion(self):
+    return self.valoracion	
+
+

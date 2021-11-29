86385131d4119d8573593ea6ed63b047b0ccc55e
Parka015 <PRM00@protonmail.com>
2021-11-29 20:51:13 +0100

    Correcciones varias #11 #10
    Corrección de los asserts de caracteristicaValida que estaban puestos al revés y cambio de parámetros de la regresion lineal para delegar la separación de características y etiqueta

--- source/funciones.py
+++ source/funciones.py
@@ -7,14 +7,16 @@ import numpy as np
 
 def caracteristicaValida(carac):
         
-        assert carac < 0 , "La caracteristica tiene que ser mayor o igual que 0"
-        assert carac > 5 , "La caracteristica tiene que ser menor o igual que 5"
+        assert carac > 0 , "La caracteristica tiene que ser mayor o igual que 0"
+        assert carac < 5 , "La caracteristica tiene que ser menor o igual que 5"
 
-def regLin(caracteristicas,etiqueta):
-    x = np.array(caracteristicas)
-    y = np.array(etiqueta)
-    
+def regLin(matriz):
+    matriz = np.array(matriz)
+
+    x = matriz[:,:len(matriz[0])-1]
+
+    y = matriz[:,len(matriz[0])-1:]
+
     pseudoinversa = np.linalg.pinv(x).dot(y)
-    pseudoinversa = pseudoinversa[:,np.newaxis]
 
     return pseudoinversa

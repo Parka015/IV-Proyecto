fa529bb4fcb260027f8c122d46774e795d678604
Parka015 <PRM00@protonmail.com>
2021-11-29 18:44:51 +0100

    Implementación regresión Lineal #12 y #11
    Siguiendo lo mencionado en #12 he usado numpy para realizar el método de la pseudoinversa para agilizar la implementación y las operaciones

--- source/funciones.py
+++ source/funciones.py
@@ -3,8 +3,18 @@
 Funciones Auxiliares
 """
 
+import numpy as np
+
 def caracteristicaValida(carac):
         
         assert carac < 0 , "La caracteristica tiene que ser mayor o igual que 0"
         assert carac > 5 , "La caracteristica tiene que ser menor o igual que 5"
 
+def regLin(caracteristicas,etiqueta):
+    x = np.array(caracteristicas)
+    y = np.array(etiqueta)
+    
+    pseudoinversa = np.linalg.pinv(x).dot(y)
+    pseudoinversa = pseudoinversa[:,np.newaxis]
+
+    return pseudoinversa

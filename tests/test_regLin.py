f1951c9aa44310a3f1d805aba81afe2280d01f3a
Parka015 <PRM00@protonmail.com>
2021-11-29 20:59:49 +0100

    Creado test para la regresión lineal
    Comprueba lo pedido en el M1 y testea #11 , debe devolver un valor entre 0 y 5, para ello se testeará con unos datos de prueba. Primero se entrena con los train_data y despues se genera la estimación del dato (serie) en test_data y finalmente se comprueba que es un valor entre 0 y 5

---
+++ tests/test_regLin.py
@@ -0,0 +1,33 @@
+import sys
+sys.path.append("..")
+
+import pytest
+import funciones_tests
+from source import funciones
+import numpy as np
+
+#Se asegura que la regresion lineal devuelve un valor entre 0 y 5
+def regLinValorCorrecto():
+    
+    #Entrada de datos de training
+    list_train = funciones_tests.leerCSV("train_data.txt")
+    list_train = funciones_tests.convertStringToFloatList(list_train)
+    
+    #Entrenamiento
+    pesos = funciones.regLin(list_train)
+    
+    #Entrada de datos de test
+    list_test = funciones_tests.leerCSV("test_data.txt")
+    list_test = funciones_tests.convertStringToFloatList(list_test)[0]
+    
+    list_test = np.array(list_test)[:len(list_test)-1]
+
+    #Calculo de la estimacion
+    estimacion = 0
+    for pos, elemento in enumerate(pesos):
+        estimacion += elemento[0] * list_test[pos]
+        
+    #Verificacion de que devuelve un valor entre 0 y 5
+    funciones.caracteristicaValida(estimacion)  #Usa asserts
+
+

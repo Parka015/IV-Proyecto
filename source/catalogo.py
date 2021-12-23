b00ddc236d20099cb27bc71efa0f1adf3ee8743f
Ignacio Carvajal Herrera <90694122+NachoCarher@users.noreply.github.com>
2021-12-23 15:27:36 +0100

    Implementada clase catalogo para organizar series
    Se implementa una clase que almacena una coleccion de objetos de la clase Serie. Facilita la organizacion de las series y el acceso a éstas.
    Se agregan los métodos agregar, mostrar y eliminar correspondientes a la lista de series. No se ha escrito código dentro de estos métodos para una posterior implementación, sin embargo, con el uso de 'pass' permitimos que el código sea válido y se pueda ejecutar. #3

---
+++ source/catalogo.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+# Clase que almacena una coleccion de series
+
+Class Catalogo:
+  
+  series = [] # Esta lista contendrá objetos de la clase Serie
+  
+  def __init__(self, series=[]):
+    self.series = series
+  
+  def agregar(self, serie):
+    pass
+    
+  def mostrar(self):
+    pass
+  
+  def eliminar(self, serie):
+    pass

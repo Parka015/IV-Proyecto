c085a615f9a310c506eee3b7395904fde078a436
Ignacio Carvajal Herrera <90694122+NachoCarher@users.noreply.github.com>
2021-12-23 11:46:44 +0100

    Implementada la predicción del éxito de una serie.
    Para alcanzar #3 es necesario poder identificar el éxito de una serie. Para ello se importa el módulo en el que se desarrolla el algoritmo predictivo en base a las características de ésta. Una vez realizadas las operaciones necesarias y calculada la estimación de la valoración, se guardará como un atributo válido (inicialmente nulo) de una serie.

--- source/serie.py
+++ source/serie.py
@@ -1,18 +1,26 @@
 # -*- coding: utf-8 -*-
 # Clase que almacena la información correspondiente a una serie
 
+import KNN
+
 Class serie:
   def __init__(self, iden):
     self.identificador = iden   # Codigo para identificar de forma unica cada serie
+    self.prediccion_valoracion = -1 # Éxito estimado de una serie. Inicialmente nulo
     
   ### Gets ### 
     
   def getId(self):
     return self.identificador
   
+  def getPrediccion(self):
+    return self.prediccion_valoracion
+  
   ### Sets ### 
 
   def setId(self, id):
     self.identificador = id
     
+  def setPrediccion(self, prediccion):
+    self.prediccion_valoracion = prediccion
     

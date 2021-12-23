ed3304cba467f0ff52824d582e1bb9601882b26d
Ignacio Carvajal Herrera <90694122+NachoCarher@users.noreply.github.com>
2021-12-23 15:10:50 +0100

    Se implementan más atributos de las series
    Con el módulo KNN se calcula una predicción de la valoración de una serie sobre sus características y una vez calculada se utiliza el respectivo método de esta clase para asignarla. No era necesario importar el módulo.
    
    Las series necesitan tener más características a parte del género (en caracteristicas.py) para poder distinguirlas y filtrarlas correctamente. Por ello se han implementado las básicas que tendría una serie. #3

--- source/serie.py
+++ source/serie.py
@@ -1,26 +1,51 @@
 # -*- coding: utf-8 -*-
 # Clase que almacena la información correspondiente a una serie
 
-import KNN
-
 Class serie:
-  def __init__(self, iden):
+  def __init__(self, iden, titulo, director, duracion, lanzamiento):
     self.identificador = iden   # Codigo para identificar de forma unica cada serie
-    self.prediccion_valoracion = -1 # Éxito estimado de una serie. Inicialmente nulo
-    
-  ### Gets ### 
+    self.prediccion = None # Éxito estimado de una serie. Inicialmente nulo
+    self.titulo = titulo
+    self.director = director
+    self.duracion = duracion
+    self.lanzamiento = lanzamiento
     
   def getId(self):
     return self.identificador
   
   def getPrediccion(self):
-    return self.prediccion_valoracion
+    return self.prediccion
   
-  ### Sets ### 
+  def getTitulo(self):
+    return self.titulo
+  
+  def getDirector(self):
+    return self.director
+  
+  def getDuracion(self):
+    return self.duracion
+  
+  def getLanzamiento(self):
+    return self.lanzamiento
 
   def setId(self, id):
     self.identificador = id
     
   def setPrediccion(self, prediccion):
-    self.prediccion_valoracion = prediccion
+    self.prediccion = prediccion
+    
+  def setTitulo(self, titulo):
+    self.titulo = titulo
+    
+  def setDirector(self, director):
+    self.director = director
+    
+  def setDuracion(self, duracion):
+    self.duracion = duracion
+    
+  def setLanzamiento(self, lanzamiento):
+    self.lanzamiento = lanzamiento
+
+    
+    
     
